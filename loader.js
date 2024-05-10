42de74fbcb5b5bdce91c2b2f62a9751989752e7d
Divy Gupta <divygupta208@gmail.com>
2024-05-10 18:15:55 +0530

    changes

--- loader.js
+++ loader.js
@@ -2,7 +2,7 @@
 
 export default function myImageLoader({ src, width, quality }) {
   if (src.startsWith("https://images.pexels.com")) return src;
-  return `https://portfolionext-gj96.vercel.app/${src}?${width}&q=${
+  return `https://portfolionext-sable.vercel.app/${src}?${width}&q=${
     quality || 75
   }`;
 }
